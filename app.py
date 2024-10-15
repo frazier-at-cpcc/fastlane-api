aa6759ece59b8a4f897ebc042ba4a4437b5df415
Frazier Smith <frazier.smith@cpcc.edu>
2024-10-15 12:15:09 -0400

    change to open port access.

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def get_product_categories():
         return jsonify({"error": "Failed to fetch data from Fast Lane API"}), response.status_code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
